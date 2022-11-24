33c85aa8882628802bbc9dca8183d48961ca1c23
Pibe <89902843+RafiPibe@users.noreply.github.com>
2022-11-24 10:51:59 +0700

    Fixing SetVisible
    bug on the game, when it runs there's just a blank screen.

--- TowerDefense/src/main/Game.java
+++ TowerDefense/src/main/Game.java
@@ -1,19 +1,42 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame{
 	
 	private GameScreen gameScreen;
 	
+	private BufferedImage img;
+	
 	// The game window
 	public Game() {
+		
+		importImg();
+		
 		setSize(640,640);
-		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		gameScreen = new GameScreen();
+		setVisible(true);
+		
+		gameScreen = new GameScreen(img);
 		add(gameScreen);
+	}
+	
+	private void importImg() {
+		InputStream is = getClass().getResourceAsStream("/sprite.png");
+		
+		// try to import the image
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			//if it goes wrong then it will print an error message
+			e.printStackTrace();
+		}
 	}
 	
 	// main function
