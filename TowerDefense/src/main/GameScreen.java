ac8d966e78ed48c1279009b5f62282ac5a8cc371
Pibe <89902843+RafiPibe@users.noreply.github.com>
2022-11-25 08:37:53 +0700

    Update GameScreen.java
    Cleaning up the codes

--- TowerDefense/src/main/GameScreen.java
+++ TowerDefense/src/main/GameScreen.java
@@ -1,7 +1,8 @@
 package main;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -9,33 +10,38 @@ import javax.swing.JPanel;
 public class GameScreen extends JPanel {
 	
 	private Random random;
+	private BufferedImage img;
 	
-	public GameScreen() {
+	private ArrayList<BufferedImage> sprites = new ArrayList<>();
+	
+	public GameScreen(BufferedImage img) {
+		this.img = img;
+		
+		loadSprites();
+		
 		random = new Random();
 	}
 	
+	private void loadSprites() {
+		for(int y = 0; y < 10; y++) {
+			for(int x = 0; x < 10; x++) {
+				sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));
+			}
+		}	
+	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		// Color for the y axis
+		// To make the sprite appear
 		for(int y = 0; y < 20; y++) {
 			for(int x = 0; x < 20; x++) {
-				// custom color can be used this way
-//				g.setColor(new Color(255, 125, 125));
-			
-				// if we want the color to be randomized, then we can use this
-				g.setColor(getRndColor());
-				g.fillRect(x * 32, y * 32, 32, 32);
+				g.drawImage(sprites.get(getRndInt()), x * 32, y * 32, null);
 			}
 		}
 	}
 	
-	// To get random color
-	private Color getRndColor() {
-		int r = random.nextInt(256); 
-		int g = random.nextInt(256); 
-		int b = random.nextInt(256);
-		
-		return new Color(r, g, b);
+	private int getRndInt() {
+		return random.nextInt(100);
 	}
 }
