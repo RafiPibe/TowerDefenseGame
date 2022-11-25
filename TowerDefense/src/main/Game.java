a9870b5ae342e14e841f96e424585216810e822c
Pibe <89902843+RafiPibe@users.noreply.github.com>
2022-11-25 08:37:13 +0700

    Update Game.java
    Cleaning up the codes

--- TowerDefense/src/main/Game.java
+++ TowerDefense/src/main/Game.java
@@ -3,15 +3,19 @@ package main;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-public class Game extends JFrame{
+public class Game extends JFrame implements Runnable {
 	
 	private GameScreen gameScreen;
-	
 	private BufferedImage img;
+	
+	private Thread gameThread;
+	
+	// FPS and UPS Cap
+	private final double FPSCap = 120.0;
+	private final double UPSCap = 60.0;
 	
 	// The game window
 	public Game() {
@@ -21,10 +25,10 @@ public class Game extends JFrame{
 		setSize(640,640);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		setVisible(true);
 		
 		gameScreen = new GameScreen(img);
 		add(gameScreen);
+		setVisible(true);
 	}
 	
 	private void importImg() {
@@ -39,10 +43,58 @@ public class Game extends JFrame{
 		}
 	}
 	
+	private void start() {
+		gameThread = new Thread(this) {};
+		gameThread.start();
+	}
+
+	private void updateGame() {
+		//System.out.println("Game Updated.");
+	}
+
 	// main function
 	public static void main(String[] args) {
 		Game game = new Game();
-		
+		game.start();
 	}
 
+	@Override
+	public void run() {
+		
+		double frameDuration = 1000000000.0 / FPSCap;
+		double updateDuration = 1000000000.0 / UPSCap;
+		
+		long lastFrame = System.nanoTime();
+		long lastUpdate = System.nanoTime();
+		
+		long lastTimeCheck = System.currentTimeMillis();
+
+		int frames = 0;
+		int updates = 0;
+		
+		// Game loop
+		while(true) {
+			//render
+			if(System.nanoTime() - lastFrame >= frameDuration) {	
+				lastFrame = System.nanoTime();
+				repaint();
+				frames++;
+			}
+			
+			//update
+			if(System.nanoTime() - lastUpdate >= updateDuration) {
+				updateGame();
+				lastUpdate = System.nanoTime();
+				updates++;
+			}
+			
+			// FPS and UPS Counter
+			if(System.currentTimeMillis() - lastTimeCheck >= 1000) {
+				System.out.println("=====================" + "\nFPS : " + frames + " | UPS : " + updates);
+				frames = 0;
+				updates = 0;
+				lastTimeCheck = System.currentTimeMillis();
+			}
+		}
+	}
 }
