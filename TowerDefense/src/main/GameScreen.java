69121528aca196ff2a331f3444a735a00a5c1b79
Pibe <89902843+RafiPibe@users.noreply.github.com>
2022-12-01 07:59:32 +0700

    Moving initInput
    Moving initInput form Game.java to GameScreen.java because of some weird bugs regarding the grids

--- TowerDefense/src/main/GameScreen.java
+++ TowerDefense/src/main/GameScreen.java
@@ -8,27 +8,52 @@ import java.util.Random;
 
 import javax.swing.JPanel;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 public class GameScreen extends JPanel {
+	
 	private Game game;
 	private Dimension size;
+	
+	private KeyboardInputs keyboardInputs;
+	private MouseInputs mouseInputs;
 	
 	public GameScreen(Game game) {
 		this.game = game;
 		
 		setPanelSize();	
+		
+	}
+	
+	public void initInputs() {
+		
+		keyboardInputs = new KeyboardInputs();
+		mouseInputs = new MouseInputs();
+		
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
+		
+		addKeyListener(keyboardInputs);
+		
+		requestFocus();
+		
 	}
 	
 	private void setPanelSize() {
+		
 		size = new Dimension(640, 640);
 		setMinimumSize(size);
 		setPreferredSize(size);
 		setMaximumSize(size);
+		
 	}
 
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
 		
+		super.paintComponent(g);
 		game.getRender().render(g);
+		
 	}
 	
 }
