78e0590c38a76de8b8d090e2d0fcf4a8633b8fc7
Pibe <89902843+RafiPibe@users.noreply.github.com>
2022-12-03 21:25:45 +0700

    Added Buttons

--- TowerDefense/src/scenes/Menu.java
+++ TowerDefense/src/scenes/Menu.java
@@ -9,7 +9,9 @@ import java.util.Random;
 
 import javax.imageio.ImageIO;
 
+import guii.Buttons;
 import main.Game;
+import static main.GameStates.*;
 
 public class Menu extends GameScene implements SceneMethods {
 
@@ -17,21 +19,39 @@ public class Menu extends GameScene implements SceneMethods {
 	private ArrayList<BufferedImage> sprites = new ArrayList<>();
 	private Random random;
 	
+	private Buttons bPlaying, bSettings, bQuit;
+	
 	public Menu(Game game) {
 		super(game);
 		random = new Random();
 		importImg();
 		loadSprites();
+		initButtons();
+	}
+
+	private void initButtons() {
+		
+		int widthButton = 150;
+		int heightButton = widthButton / 3;
+		int x = 640 / 2 - widthButton / 2;
+		int y = 150;
+		int ySpace = 100;
+		
+		bPlaying = new Buttons("Play", x, y, widthButton, heightButton);
+		bSettings = new Buttons("Settings", x, y + ySpace, widthButton, heightButton);
+		bQuit = new Buttons("Quit", x, y + ySpace * 2, widthButton, heightButton);
 	}
 
 	@Override
 	public void render(Graphics g) {
-		// To make the sprite appear
-		for(int y = 0; y < 20; y++) {
-			for(int x = 0; x < 20; x++) {
-				g.drawImage(sprites.get(getRndInt()), x * 32, y * 32, null);
-			}
-		}
+		drawButtons(g);
+	}
+
+	private void drawButtons(Graphics g) {
+		bPlaying.draw(g);
+		bSettings.draw(g);
+		bQuit.draw(g);
+		
 	}
 
 	private void importImg() {
@@ -56,5 +76,57 @@ public class Menu extends GameScene implements SceneMethods {
 	
 	private int getRndInt() {
 		return random.nextInt(100);
+	}
+
+	@Override
+	public void mouseClicked(int x, int y) {
+		if(bPlaying.getBounds().contains(x, y)) {
+			SetGameState(PLAYING);
+		}
+		else if (bSettings.getBounds().contains(x, y)) {
+			SetGameState(SETTINGS);
+		}
+		else if (bQuit.getBounds().contains(x, y)) {
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void mouseMoved(int x, int y) {
+		bPlaying.setMouseOver(false);
+		bSettings.setMouseOver(false);
+		bQuit.setMouseOver(false);
+
+		if (bPlaying.getBounds().contains(x, y)) {
+			bPlaying.setMouseOver(true);
+		}
+		else if (bSettings.getBounds().contains(x, y)) {
+			bSettings.setMouseOver(true);
+		}	
+		else if (bQuit.getBounds().contains(x, y)) {
+			bQuit.setMouseOver(true);
+		}
+	}
+
+	@Override
+	public void mousePressed(int x, int y) {
+		
+		if(bPlaying.getBounds().contains(x, y)) {
+			bPlaying.setMousePressed(true);
+		}
+		else if(bSettings.getBounds().contains(x, y)) {
+			bSettings.setMousePressed(true);
+		}
+		else if(bQuit.getBounds().contains(x, y)) {
+			bQuit.setMousePressed(true);
+		}
+	}
+
+	@Override
+	public void mouseReleased(int x, int y) {
+		bPlaying.resetBool();
+		bSettings.resetBool();
+		bQuit.resetBool();
+		
 	}
 }
