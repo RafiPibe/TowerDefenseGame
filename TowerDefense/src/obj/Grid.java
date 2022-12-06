04d157329691ea8ba8d9f1609a2c488a6dc2ef0d
Pibe <89902843+RafiPibe@users.noreply.github.com>
2022-12-06 23:36:24 +0700

    Updated Grid

--- TowerDefense/src/obj/Grid.java
+++ TowerDefense/src/obj/Grid.java
@@ -5,12 +5,28 @@ import java.awt.image.BufferedImage;
 public class Grid {
 	
 	private BufferedImage sprite;
+	private int id;
+	private String name;
 	
-	public Grid(BufferedImage sprite) {
+	public Grid(BufferedImage sprite, int id, String name) {
 		this.sprite = sprite;
+		this.id = id;
+		this.name = name;
 	}
 	
+	// Getter
 	public BufferedImage getSprite() {
 		return sprite;
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	
 }
